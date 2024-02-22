66bdaa237e2d5318f6bc3366930ecc85a57b83f2
Precious Olusola <preciousolusola16@gmail.com>
2024-02-22 07:29:09 +0100

    Update update-env script

--- scripts/dev/update-env-example.sh
+++ scripts/dev/update-env-example.sh
@@ -5,13 +5,17 @@ source "$(dirname "$0")/functions.sh"
 base_dir=$(getProjectDir)
 
 env_path="$base_dir/env/.env.development"
-env_dest_path="$base_dir/env/.env.example"
+relative_path="env/.env.example"
+env_dest_path="$base_dir/$relative_path"
 
 # Check if the development env exist
 if [ -e "$env_path" ]; then
     # Extract the variable name into .env.example
     awk -F= '/^[A-Za-z_][A-Za-z0-9_]*=/ {print $1 "="}' $env_path > $env_dest_path
-    echo "Created template environment variable at: $env_dest_path"
+    echo "Successfully set environement variable template at: $env_dest_path"
+
+    git add "$relative_path"
+    git commit -m "Updated .env.example file"
 else
     echo "Development env file does not exist, exiting."
     exit 1
